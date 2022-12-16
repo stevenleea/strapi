f8d9fdd6f83da9b6b886619ce82672448e909210
najibean <najibullah89@gmail.com>
2022-12-16 15:14:05 +0800

    add Afterplays

---
+++ src/api/afterplay/controllers/afterplay.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  afterplay controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::afterplay.afterplay');
---
+++ src/api/afterplay/routes/afterplay.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * afterplay router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::afterplay.afterplay');
